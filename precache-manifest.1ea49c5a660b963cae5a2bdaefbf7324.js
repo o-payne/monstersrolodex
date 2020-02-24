68ee55bc2228db3bfd97cf405035768d73d9d748
Owen <owen.payne42@gmail.com>
2020-02-24 15:48:51 -0500

    Updates

---
+++ precache-manifest.1ea49c5a660b963cae5a2bdaefbf7324.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feef8d2cfcbf3ff8c24fe2a01c21f6b6",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "14c97041fd60db585b25",
+    "url": "/monstersrolodex/static/css/main.94825b71.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monstersrolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monstersrolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c97041fd60db585b25",
+    "url": "/monstersrolodex/static/js/main.7afe4a9f.chunk.js"
+  },
+  {
+    "revision": "7866796e6fc4260a18cc",
+    "url": "/monstersrolodex/static/js/runtime-main.dbd6355d.js"
+  }
+]);
